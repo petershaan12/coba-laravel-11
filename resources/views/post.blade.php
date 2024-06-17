4697f2374a7cb063e8a2bc8a0cec9b5dbdfefabb
petershaan12 <2181002@unai.edu>
2024-06-17 20:01:43 +0700

    post category episode 12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,12 +2,14 @@
     <x-slot:title>Single Post</x-slot:title>
     <article class="py-8 max-w-screen-md">
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">{{ $post['title'] }}</h2>
-        <div class="text-base text-gray-500">
-            By 
-            <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a>
-            in <a href="">Web programming</a> | {{ $post->created_at->diffForHumans() }}
-
-        </div>
+            <div>
+                By 
+                <a href="/authors/{{ $post->author->username }}" class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500">
+                    {{ $post->category->name }}
+                </a> | {{ $post->created_at->diffForHumans() }}
+           
+            </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Post </a>
     </article>
